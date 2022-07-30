c5a048e42cced9f6d4b38e121badc625e79a8362
sohana <sshabnamzsf@gmail.com>
2022-07-30 12:23:12 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function validation()
+{
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if(username==="soha" && password==="sohana")
+    {
+        alert("login successfully");
+    }
+    else{
+        alert("login failed");
+    }
+}
